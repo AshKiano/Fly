ed54b7101425cb89461c14e9f6e9044ad77b4dca
jvyskocil <ash.kiano@gmail.com>
2024-07-02 02:05:10 +0200

    version 1.3

--- src/main/java/com/ashkiano/fly/Fly.java
+++ src/main/java/com/ashkiano/fly/Fly.java
@@ -5,6 +5,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Fly extends JavaPlugin implements CommandExecutor {
 
@@ -13,6 +19,7 @@ public class Fly extends JavaPlugin implements CommandExecutor {
         this.getCommand("fly").setExecutor(this);
         Metrics metrics = new Metrics(this, 21926);
         this.getLogger().info("Thank you for using the Fly plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+        checkForUpdates();
     }
 
     @Override
@@ -33,5 +40,45 @@ public class Fly extends JavaPlugin implements CommandExecutor {
         player.setAllowFlight(!flying);
         player.sendMessage("Flying " + (!flying ? "enabled" : "disabled"));
         return true;
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 }
